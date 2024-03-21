987376863951b8bc60604d64738313350a101604
Lukinhas Do Javinha <143123619+LuquinhasDoJava@users.noreply.github.com>
2024-03-20 21:19:37 -0300

    Update Main.java

--- aCulpaDasEstrelas/src/view/Main.java
+++ aCulpaDasEstrelas/src/view/Main.java
@@ -8,8 +8,9 @@ import java.util.concurrent.Semaphore;
 public class Main {
     public static void main(String[] args) {
         Semaphore semaphore = new Semaphore(1);
+        Semaphore semaphore1 = new Semaphore(1);
         for(int x = 0; x<10; x++){
-            ThreadsCoocked threadsCoocked = new ThreadsCoocked(semaphore);
+            ThreadsCoocked threadsCoocked = new ThreadsCoocked(semaphore,semaphore1);
             threadsCoocked.start();
     }
     }
